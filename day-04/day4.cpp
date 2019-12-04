e74270d47d674fc00ee755da73f9d3ff2588a1a5
WhoSoup <who.soup@gmail.com>
2019-12-04 11:38:36 +0100

    cleanup

--- day-04/day4.cpp
+++ day-04/day4.cpp
@@ -1,65 +1,43 @@
 #include <iostream>
 #include <vector>
 
-bool valid(int pw) {
+std::pair<bool,bool> valid(int pw) {
     bool repeat = false;
+    bool repeat2 = false;
+    int run = 0;
     int prev = 10; // going reverse, so higher than 9
     while (pw > 0) {
         int r = pw % 10;
 
+        if (prev < r) {
+            return std::pair<bool,bool>{false,false};
+        }
+
         if (prev == r) {
             repeat = true;
-        }
-        if (prev < r) {
-            return false;
-        }
-
-        pw /= 10;
-        prev = r;
-    }
-
-    return repeat;
-}
-
-bool valid2(int pw) {
-
-    int prev = 10;
-    int run = 0;
-    bool repeat = false;
-    while (pw > 0) {
-        int r = pw % 10;
-        
-        if (r == prev) {
             run++;
         } else {
             if (run == 1) {
-                repeat = true;
+                repeat2 = true;
             }
             run = 0;
-        }
-        if (prev < r) {
-            return false;
         }
 
         pw /= 10;
         prev = r;
     }
 
-    return repeat || run == 1;
+    return std::pair<bool,bool>{repeat, repeat2 || run == 1};
 }
-
 
 int main() {
     int start = 353096, end = 843212;
 
     int v = 0, v2 = 0;
     for (int i = start; i <= end; i++) {
-        if (valid(i)) {
-            v++;
-        }
-        if (valid2(i)) {
-            v2++;
-        } 
+        auto p = valid(i);
+        if (p.first) v++;
+        if (p.second) v2++;
     }
     std::cout << v << std::endl;
     std::cout << v2 << std::endl;
